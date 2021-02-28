3446b7b1d8efbae8d6b7c2339e67b21f154b894b
tmz0120 <602832450@qq.com>
2021-03-01 00:50:28 +0800

    修改代码的备注

--- testcase/testlogin.py
+++ testcase/testlogin.py
@@ -1,1 +1,14 @@
-print("测试")
+
+class Testlogincase:
+
+    def setuo(self):
+        print("前置条件")
+
+    def teardown(self):
+        print("后置条件")
+
+    def test_01(self):
+        assert 1+1==2
+
+    def test_02(self):
+        assert 1+1==3
